d95ef20bdf67da5034b8e2caea02652dcaa9b293
Zach YU <git@yuz.me>
2014-04-19 18:59:59 +0200

    add smooth-scroll.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,11 @@
 $(document).ready(function() {
 	$('.invis').hide();
-	$('.more').on('click', function(e) {
-		e.preventDefault();
+	$('.more').on('click', function(event) {
+		event.preventDefault();
 		var href = $(this).attr('href');
 		if ($('#ajax').is(':visible')) {
 			$('#ajax').css({ display:'block' }).empty();
 		}
 		$('#ajax').html($(href).html()).hide().fadeIn('slow');
-	});
-
-	$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1500);
-        return false;
-      }
-    }
 	});
 });
