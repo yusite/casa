62d78382fa090feae31e254ab742b33b6e40ae3d
Zach YU <git@yuz.me>
2014-04-19 16:17:47 +0200

    add smooth scroll

--- js/main.js
+++ js/main.js
@@ -8,4 +8,17 @@ $(document).ready(function() {
 		}
 		$('#ajax').html($(href).html()).hide().fadeIn('slow');
 	});
+
+	$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+	});
 });
