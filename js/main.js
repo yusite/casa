d4b465689eda8e89ef319b023c38dddc848182f3
Zach YU <git@yuz.me>
2014-04-19 18:34:38 +0200

    minor changes

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, 1500);
         return false;
       }
     }
